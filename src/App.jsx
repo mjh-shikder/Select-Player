b48dc857a0ddc2a209f759bafb3df2711b32e1ee
mjh-shikder <mjhsikder@gmail.com>
2025-09-25 14:10:53 +0600

    cart and dynamic information added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
-import { useState } from 'react'
-import './App.css'
-import navImg from './assets/images/logo.png'
-import coin from './assets/images/Currency.png'
+import { Suspense, useState } from "react";
+import "./App.css";
+import navImg from "./assets/images/logo.png";
+import coin from "./assets/images/Currency.png";
+import AvailablePlayer from "./components/AvailablePlayers/AvailablePlayer";
+import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
 
-function App() {
+
+const fetchPlayers = async () => {
+  const res = await fetch('/players.json')
+  return res.json()
   
-
-  return (
-    <>
-     <div className="navbar w-11/12 mx-auto ">
-  <div className="flex-1">
-          <a className=" text-xl">
-            <img className='w-[60px] h-[60px] ' src={navImg} alt="" />
-    </a>
-  </div>
-  <div className="flex items-center">
-    <span className='mr-2'>60000000000</span>
-          <span className='mr-2'>Coins </span>
-          <img src={coin} alt="" />
-  </div>
-</div>
-    </>
-  )
 }
 
-export default App
+function App() {
+  const playerPromise = fetchPlayers()
+  return (
+    <>
+      {/* Nabar */}
+      <div className="navbar w-11/12 mx-auto ">
+        <div className="flex-1">
+          <a className=" text-xl">
+            <img className="w-[60px] h-[60px] " src={navImg} alt="" />
+          </a>
+        </div>
+        <div className="flex items-center">
+          <span className="mr-2">60000000000</span>
+          <span className="mr-2">Coins </span>
+          <img src={coin} alt="" />
+        </div>
+      </div>
+      {/*  */}
+      <div>
+        <h2>Available Players </h2>
+        <div>
+          <button></button>
+        </div>
+    </div>
+
+      <Suspense fallback={ <span className="loading loading-spinner loading-xl"></span>}>
+        <AvailablePlayer playerPromise={playerPromise}></AvailablePlayer>
+     </Suspense>
+      
+
+
+      <SelectedPlayers></SelectedPlayers>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div className='w-11/12 mx-auto '>
+            Selected 
+        </div>
+    );
+};
+
+export default SelectedPlayers;

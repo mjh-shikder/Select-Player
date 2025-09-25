8405713c532c0d2ae8f34ae35733d5b2adfb141e
mjh-shikder <mjhsikder@gmail.com>
2025-09-25 19:21:54 +0600

    Player cards moved to PlayerCard Component

--- src/components/AvailablePlayers/AvailablePlayer.jsx
+++ src/components/AvailablePlayers/AvailablePlayer.jsx
@@ -1,6 +1,6 @@
 import React, { use } from "react";
-import profileImg from "../../assets/images/profile.png";
-import flag from "../../assets/images/flag.png";
+
+import PlayerCard from "../PlayerCard/PlayerCard";
 
 const AvailablePlayer = ({ playerPromise }) => {
   const playerData = use(playerPromise);
@@ -9,39 +9,7 @@ const AvailablePlayer = ({ playerPromise }) => {
   return (
     <div className="w-11/12 mx-auto ">
       <div className="md:grid  md:grid-cols-2 lg:grid-cols-3 gap-3 ">
-        {playerData.map((player) => (
-          <div className="card bg-base-100  shadow-sm p-4 ">
-            <figure>
-                    <img
-              className="w-full h-[300px] object-cover rounded-b-lg "          src={player["playerImage"]} alt="Shoes" />
-            </figure>
-            <div className="mt-4  ">
-              <div className="flex space-x-3 ">
-                <img src={profileImg} alt="" />{" "}
-                <h2 className="card-title  ">{player.playerName}</h2>
-              </div>
-              <div className="flex justify-between mt-4 border-b-1 border-gray-400 pb-2 ">
-                <div className="flex items-center space-x-2  ">
-                  <img className="w-[20px] h-[20px] " src={flag} alt="" />
-                  <span>{player.playerCountry}</span>
-                </div>
-
-                <button className="btn ">{player.playingRole}</button>
-              </div>
-              <div>
-                <h2 className="font-bold ">Rating: {player.rating}</h2>
-                <div className="flex justify-between mb-2 mt-2 ">
-                  <h2 className="font-bold">{player.battingStyle}</h2>
-                  <h2 className="text-gray-500 ">{player.bowlingStyle}</h2>
-                </div>
-                <div className="flex justify-between ">
-                            <h2 className="font-bold">Price: ${ player.price}</h2>
-                  <button className="btn ">Choose Player</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+        {playerData.map((player) =><PlayerCard player={player}></PlayerCard> )}
       </div>
     </div>
   );
---
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import profileImg from "../../assets/images/profile.png";
+import flag from "../../assets/images/flag.png";
+
+const PlayerCard = ({player}) => {
+    return (
+        <div className="card bg-base-100  shadow-sm p-4 ">
+            <figure>
+                    <img
+              className="w-full h-[300px] object-cover rounded-b-lg "          src={player["playerImage"]} alt="Shoes" />
+            </figure>
+            <div className="mt-4  ">
+              <div className="flex space-x-3 ">
+                <img src={profileImg} alt="" />{" "}
+                <h2 className="card-title  ">{player.playerName}</h2>
+              </div>
+              <div className="flex justify-between mt-4 border-b-1 border-gray-400 pb-2 ">
+                <div className="flex items-center space-x-2  ">
+                  <img className="w-[20px] h-[20px] " src={flag} alt="" />
+                  <span>{player.playerCountry}</span>
+                </div>
+
+                <button className="btn ">{player.playingRole}</button>
+              </div>
+              <div>
+                <h2 className="font-bold ">Rating: {player.rating}</h2>
+                <div className="flex justify-between mb-2 mt-2 ">
+                  <h2 className="font-bold">{player.battingStyle}</h2>
+                  <h2 className="text-gray-500 ">{player.bowlingStyle}</h2>
+                </div>
+                <div className="flex justify-between ">
+                            <h2 className="font-bold">Price: ${ player.price}</h2>
+                  <button className="btn ">Choose Player</button>
+                </div>
+              </div>
+            </div>
+          </div>
+    );
+};
+
+export default PlayerCard;
